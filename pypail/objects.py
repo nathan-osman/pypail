794f28fa426b1d6fb41b1c55392f376f41bde1f7
Nathan Osman <admin@quickmediasolutions.com>
2014-09-07 19:32:36 -0700

    Implemented basic fetching of droplet attributes.

--- pypail/objects.py
+++ pypail/objects.py
@@ -7,6 +7,10 @@ class Object(object):
     objects contain an embedded Region, which contains one or more Sizes.
     """
 
+    _EMBED = {}  # derived classes override these
+    _FETCH = {}
+    _DATES = []
+
     def __init__(self, credentials, data=None):
         self._credentials, self._data = credentials, data
 
@@ -14,21 +18,56 @@ class Object(object):
         return '<%s>' % self.__class__.__name__
 
     def __getattr__(self, key):
+        if key in self._EMBED:
+            return self._EMBED[key]()(self._credentials, self._data[key])
+        elif key in self._FETCH:
+            return self._fetch(
+                self._FETCH[key]['endpoint'](self),
+                self._FETCH[key]['class'](),
+            )
         return self._data[key]
 
-    def fetch(self, endpoint, class_):
-        for data in self._credentials.request(endpoint)[class_.MULTIPLE_KEY]:
+    def _fetch(self, endpoint, class_):
+        for data in self._credentials.request(endpoint)[class_._MULTIPLE_KEY]:
             yield class_(self._credentials, data)
 
 
 class Account(Object):
 
-    @property
-    def droplets(self):
-        return self.fetch('/droplets', Droplet)
+    _FETCH = {
+        'droplets': {
+            'endpoint': lambda x: '/droplets',
+            'class': lambda: Droplet,
+        },
+    }
 
 
 class Droplet(Object):
 
-    SINGLE_KEY = 'droplet'
-    MULTIPLE_KEY = 'droplets'
+    _SINGLE_KEY = 'droplet'
+    _MULTIPLE_KEY = 'droplets'
+
+    _EMBED = {
+        'image': lambda: Image,
+        'kernel': lambda: Kernel,
+        'region': lambda: Region,
+        'size': lambda: Size,
+    }
+
+    _DATES = ['created_at']
+
+
+class Image(Object):
+    pass
+
+
+class Kernel(Object):
+    pass
+
+
+class Region(Object):
+    pass
+
+
+class Size(Object):
+    pass
