30f6f6b78c9ff690547670d9ebc35cd93c4817a5
Nathan Osman <admin@quickmediasolutions.com>
2014-09-09 23:19:55 -0700

    Implemented special properties for most items.

--- pypail/objects.py
+++ pypail/objects.py
@@ -1,3 +1,6 @@
+from dateutil.parser import parse
+
+
 class Object(object):
     """
     Generic object returned by the API.
@@ -7,9 +10,7 @@ class Object(object):
     objects contain an embedded Region, which contains one or more Sizes.
     """
 
-    _EMBED = {}  # derived classes override these
-    _FETCH = {}
-    _DATES = []
+    _SPECIAL = {}  # derived classes override these
 
     def __init__(self, credentials, data=None):
         self._credentials, self._data = credentials, data
@@ -18,56 +19,125 @@ class Object(object):
         return '<%s>' % self.__class__.__name__
 
     def __getattr__(self, key):
-        if key in self._EMBED:
-            return self._EMBED[key]()(self._credentials, self._data[key])
-        elif key in self._FETCH:
-            return self._fetch(
-                self._FETCH[key]['endpoint'](self),
-                self._FETCH[key]['class'](),
-            )
-        return self._data[key]
+        return self._SPECIAL[key](self) if key in self._SPECIAL else self._data[key]
 
-    def _fetch(self, endpoint, class_):
-        for data in self._credentials.request(endpoint)[class_._MULTIPLE_KEY]:
-            yield class_(self._credentials, data)
+    def _date(self, key):
+        """
+        Convert an ISO 8601 date to datetime.
+        """
+        return parse(self._data[key])
+
+    def _filter(self, endpoint, class_, key, condition):
+        """
+        Return a generator that filters based on a condition.
+        """
+        for item in self._multi(endpoint, class_, key):
+            if condition(item):
+                yield item
+
+    def _multi(self, endpoint, class_, key):
+        """
+        Return a generator for items at the specified endpoint.
+        """
+        for item in self._credentials.request(endpoint)[key]:
+            yield class_(self._credentials, item)
+
+    def _single(self, class_, key):
+        """
+        Return an embedded instance of the specified class.
+        """
+        return class_(self._credentials, self._data[key])
 
 
 class Account(Object):
 
-    _FETCH = {
-        'droplets': {
-            'endpoint': lambda x: '/droplets',
-            'class': lambda: Droplet,
-        },
+    _SPECIAL = {
+        'actions': lambda x: x._multi('/actions', Action, 'actions'),
+        'domains': lambda x: x._multi('/domains', Domain, 'domains'),
+        'droplets': lambda x: x._multi('/droplets', Droplet, 'droplets'),
+        'images': lambda x: x._multi('/images', Image, 'images'),
+        'keys': lambda x: x._multi('/account/keys', SSHKey, 'ssh_keys'),
+        'regions': lambda x: x._multi('/regions', Region, 'regions'),
+        'sizes': lambda x: x._multi('/sizes', Size, 'sizes'),
     }
+
+
+class Action(Object):
+
+    _SPECIAL = {
+        'completed_at': lambda x: x._date('completed_at'),
+        'region': lambda x: x._filter('/regions', Region, 'regions', lambda y: x._data['region'] == y.slug).next(),
+        'started_at': lambda x: x._date('started_at'),
+    }
+
+    # TODO: implement a 'resource' property that automatically
+    # fetches the appropriate resource based on ID and type
+
+
+class Backup(Object):
+    pass
+
+
+class Domain(Object):
+
+    _SPECIAL = {
+        'records': lambda x: x._multi('/domains/%s/records' % x.name, DomainRecord, 'domain_records'),
+    }
+
+
+class DomainRecord(Object):
+
+    pass
 
 
 class Droplet(Object):
 
-    _SINGLE_KEY = 'droplet'
-    _MULTIPLE_KEY = 'droplets'
-
-    _EMBED = {
-        'image': lambda: Image,
-        'kernel': lambda: Kernel,
-        'region': lambda: Region,
-        'size': lambda: Size,
+    _SPECIAL = {
+        'actions': lambda x: x._multi('/droplets/%d/actions' % x.id, Action, 'actions'),
+        'backups': lambda x: x._multi('/droplets/%d/backups' % x.id, Backup, 'backups'),
+        'created_at': lambda x: x._date('created_at'),
+        'image': lambda x: x._single(Image, 'image'),
+        'kernel': lambda x: x._single(Kernel, 'kernel'),
+        'kernels': lambda x: x._multi('/droplets/%d/kernels' % x.id, Kernel, 'kernels'),
+        'region': lambda x: x._single(Region, 'region'),
+        'size': lambda x: x._single(Size, 'size'),
+        'snapshots': lambda x: x._multi('/droplets/%d/snapshots' % x.id, Snapshot, 'snapshots'),
     }
-
-    _DATES = ['created_at']
 
 
 class Image(Object):
-    pass
+
+    _SPECIAL = {
+        'actions': lambda x: x._multi('/images/%d/actions' % x.id, Action, 'actions'),
+        'created_at': lambda x: x._date('created_at'),
+        'regions': lambda x: x._filter('/regions', Region, 'regions', lambda y: y.slug in x._data['regions']),
+    }
 
 
 class Kernel(Object):
+
     pass
 
 
 class Region(Object):
-    pass
+
+    _SPECIAL = {
+        'sizes': lambda x: x._filter('/sizes', Size, 'sizes', lambda y: y.slug in x._data['sizes']),
+    }
 
 
 class Size(Object):
+
+    _SPECIAL = {
+        'regions': lambda x: x._filter('/regions', Region, 'regions', lambda y: y.slug in x._data['regions']),
+    }
+
+
+class Snapshot(Object):
+
+    pass
+
+
+class SSHKey(Object):
+
     pass
